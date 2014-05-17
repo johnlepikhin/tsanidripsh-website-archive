516f145558b2d9f79c4c222d53ec1ee702139765
Evgenii Lepikhin <johnlepikhin@gmail.com>
2014-05-17 12:45:30 +0400

    1. Теперь галерея перерисовывается, если ширина браузера заметно изменилась. 2. Убрал идиотизм с большой кнопкой закрытия, добавил аккуратную в угол.

---
+++ install_static.ml
@@ -0,0 +1,18 @@
+open Static
+
+let src_full_path p = Printf.sprintf "%s%s" Config.static_path (Purl.to_string p)
+
+let dst_full_path p = Printf.sprintf "%s%s" Config.save_path (Purl.to_string p)
+
+let create_all () =
+	let f t =
+		let src_full = src_full_path t.src in
+		let dst_full = dst_full_path t.dst in
+		if not (Sys.file_exists src_full) then
+			raise (failwith (Printf.sprintf "Static file '%s' not found" src_full));
+		let _ = Sys.command (Printf.sprintf "cp '%s' '%s' && chmod 644 '%s'" src_full dst_full dst_full) in
+		()
+	in
+	List.iter f !files
+
+
