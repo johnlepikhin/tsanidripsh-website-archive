a95506f16507dd4b152a448084ccdc500d329333
Evgenii Lepikhin <johnlepikhin@gmail.com>
2014-05-22 00:04:00 +0400

    bugfix

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -36,6 +36,7 @@ let dispatcher hook =
 					let binary = "../" ^ binary in
 					let args = List.map (fun a -> A a) args in
 					Cmd (A binary :: args)
+				in
 
 				rule "Install static" ~prod:"install.static" ~deps:install_deps (cmd_install []);
 
