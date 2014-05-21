2f844de9eb1ebfa03a005e104ba9ab8c3a3e5856
Evgenii Lepikhin <johnlepikhin@gmail.com>
2014-05-21 23:56:35 +0400

    Оптимизировал myocamlbuild

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -14,28 +14,32 @@ let dispatcher hook =
 	begin
 		match hook with
 			| After_rules ->
+				(* JS *)
 				rule_cp "Copy JS" ~prod:"copy.js_main" "js_main.js" "../static/";
 
+				(* Gallery *)
 				rule "Generate gallery_info.ml" ~prods:["generate.gallery_info"; "gallery_info.ml"] (fun env builder ->
 					Cmd (A "../make_galleryinfo")
 				);
 
+				(* CSS *)
 				rule "Generate s.css" ~prods:["generate.css"; "s.css"] ~dep:"source.s.css" (fun env builder ->
 					Cmd (S[A"sh"; A"-c"; A"ccss <source.s.css > s.css.uncompressed && yui-compressor --type=css < s.css.uncompressed > s.css"])
 				);
 				rule_cp "Copy CSS" ~prod:"copy.css" "s.css" "../static/";
 
-				rule "Install static" ~prod:"install.static" ~deps:["generate.gallery_info"; "copy.js_main"; "copy.css"; binary] (fun env builder ->
-					let open Command in
-					let binary = "../" ^ binary in
-					Cmd (A binary)
-				);
+				(* Install *)
+				let install_deps = ["generate.gallery_info"; "copy.js_main"; "copy.css"; binary] in
 
-				rule "Install with gallery" ~prod:"install.with_gallery" ~deps:["generate.gallery_info"; "copy.js_main"; "copy.css"; binary] (fun env builder ->
+				let cmd_install args env builder =
 					let open Command in
 					let binary = "../" ^ binary in
-					Cmd (S[A binary; A "gallery"])
-				);
+					let args = List.map (fun a -> A a) args in
+					Cmd (A binary :: args)
+
+				rule "Install static" ~prod:"install.static" ~deps:install_deps (cmd_install []);
+
+				rule "Install with gallery" ~prod:"install.with_gallery" ~deps:install_deps (cmd_install ["gallery"]);
 			| _ -> ()
 	end;
 	Ocamlbuild_js_of_ocaml.dispatcher hook
