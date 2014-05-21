f7d406ad6868c8ea043d6c55b9521e2de0bb0ca8
Evgenii Lepikhin <johnlepikhin@gmail.com>
2014-05-22 00:36:49 +0400

    Переделал генерацию CSS

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -7,15 +7,15 @@ let cmd_copy file dest =
 	let open Command in
 	Cmd (S[A"cp"; P file; P dest])
 
-let rule_cp name ~prod file dest =
-	rule name ~prod ~dep:file (fun env builder -> cmd_copy (Pathname.mk file) (Pathname.mk dest))
+let rule_cp ~prod file dest =
+	rule (Printf.sprintf "Copy %s" file) ~prod ~dep:file (fun env builder -> cmd_copy (Pathname.mk file) (Pathname.mk dest))
 
 let dispatcher hook =
 	begin
 		match hook with
 			| After_rules ->
 				(* JS *)
-				rule_cp "Copy JS" ~prod:"copy.js_main" "js_main.js" "../static/";
+				rule_cp ~prod:"copy.js.main_js" "js_main.js" "../static/";
 
 				(* Gallery *)
 				rule "Generate gallery_info.ml" ~prods:["generate.gallery_info"; "gallery_info.ml"] (fun env builder ->
@@ -23,13 +23,18 @@ let dispatcher hook =
 				);
 
 				(* CSS *)
-				rule "Generate s.css" ~prods:["generate.css"; "s.css"] ~dep:"source.s.css" (fun env builder ->
-					Cmd (S[A"sh"; A"-c"; A"ccss <source.s.css > s.css.uncompressed && yui-compressor --type=css < s.css.uncompressed > s.css"])
+				rule "Generate uncompressed.s.css" ~prod:"uncompressed.css.s" ~dep:"source.s.css" (fun env builder ->
+					Cmd (S[A"sh"; A"-c"; A"ccss <source.s.css > uncompressed.s.css"])
 				);
-				rule_cp "Copy CSS" ~prod:"copy.css" "s.css" "../static/";
+
+				rule "Generate s.css" ~prod:"s.css" ~dep:"uncompressed.css.s" (fun env builder ->
+					Cmd (S[A"yui-compressor"; A"--type=css"; A"-o"; A"s.css"; A"uncompressed.s.css"])
+				);
+
+				rule_cp ~prod:"copy.css.s" "s.css" "../static/";
 
 				(* Install *)
-				let install_deps = ["generate.gallery_info"; "copy.js_main"; "copy.css"; binary] in
+				let install_deps = ["generate.gallery_info"; "copy.js.main_js"; "copy.css.s"; binary] in
 
 				let cmd_install args env builder =
 					let open Command in
