47f386a5b8396627aca736a8dc60d0dbc1cd6523
Evgenii Lepikhin <johnlepikhin@gmail.com>
2019-02-02 21:51:44 +0300

    Minor changes

--- p_a_3d_tsandripsh_beach.ml
+++ p_a_3d_tsandripsh_beach.ml
@@ -2,15 +2,17 @@
 open Article
 module Html = Tyxml.Html
 
-let tsandripsh = make ~path:Page_common.a_tsandripsh_beach_3d ~title:"3D: пляж Цандрипша" [%html{|
-	<div class="tpl_main_center">
-		$Tpl_pano360.pano360 ~title:"По пути к пляжу" Static.pano_tsandripsh_beach1$
-		$Tpl_pano360.hint$
-		$Tpl_pano360.pano360 ~title:"Основной пляж (от нашего дома 7-10 минут)" Static.pano_tsandripsh_beach2$
-		$Tpl_pano360.hint$
-		$Tpl_pano360.pano360 ~title:"Пляж 'Белые скалы' (от нашего дома 25 минут)" Static.pano_tsandripsh_beach3$
-		$Tpl_pano360.hint$
-	</div>
-|}]
+let tsandripsh = make ~path:Page_common.a_tsandripsh_beach_3d ~title:"3D: пляж Цандрипша" [%html
+	"<div class='tpl_main_center'>"
+		[
+			Tpl_pano360.pano360 ~title:"По пути к пляжу" Static.pano_tsandripsh_beach1;
+			Tpl_pano360.hint;
+			Tpl_pano360.pano360 ~title:"Основной пляж (от нашего дома 7-10 минут)" Static.pano_tsandripsh_beach2;
+			Tpl_pano360.hint;
+			Tpl_pano360.pano360 ~title:"Пляж 'Белые скалы' (от нашего дома 25 минут)" Static.pano_tsandripsh_beach3;
+			Tpl_pano360.hint;
+		]
+	"</div>"
+]
 
 
